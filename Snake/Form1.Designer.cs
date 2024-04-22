8ff8be304d0e6c6b2571587ff7d541c8e056d3dc
vny <vny@98A4>
2024-04-22 22:36:40 +0300

    Добавьте файлы проекта.

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cube = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cube)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cube
+            // 
+            this.cube.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.cube.Location = new System.Drawing.Point(671, 365);
+            this.cube.Name = "cube";
+            this.cube.Size = new System.Drawing.Size(100, 100);
+            this.cube.TabIndex = 0;
+            this.cube.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(877, 278);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 2);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1831, 965);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cube);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cube)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox cube;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        private int _width = 800;
+        private int _height = 800;
+        private int _sizeOfSides;
+        public Form1()
+        {
+            this.Width = _width;
+            this.Height = _height;
+            InitializeComponent();
+            _generateMap();
+            this.KeyDown += new KeyEventHandler(OKP);
+        }
+
+       private void _generateMap()
+        {
+
+        }
+
+        private void OKP(object sender, KeyEventArgs e)
+        {
+           switch(e.KeyCode.ToString())
+            {
+                case "Right":
+                    cube.Location = new Point(cube.Location.X + 20, cube.Location.Y);
+                    break;
+                case "Left":
+                    cube.Location = new Point(cube.Location.X - 20, cube.Location.Y);
+                    break;
+                case "Up":
+                    cube.Location = new Point(cube.Location.X, cube.Location.Y - 20);
+                    break;
+                case "Down":
+                    cube.Location = new Point(cube.Location.X, cube.Location.Y + 20);
+                    break;
+            }
+        }
+    }
+}
