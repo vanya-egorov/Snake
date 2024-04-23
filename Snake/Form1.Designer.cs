b196033a6e4bd6974a86c92501103c5d0cdea7b7
vny <vny@98A4>
2024-04-23 03:21:38 +0300

    first commit

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -28,38 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cube = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.cube)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
-            // 
-            // cube
-            // 
-            this.cube.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.cube.Location = new System.Drawing.Point(671, 365);
-            this.cube.Name = "cube";
-            this.cube.Size = new System.Drawing.Size(100, 100);
-            this.cube.TabIndex = 0;
-            this.cube.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(877, 278);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(80, 2);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(1831, 965);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.cube);
+            this.ClientSize = new System.Drawing.Size(866, 706);
             this.Name = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.cube)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,8 +43,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox cube;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
